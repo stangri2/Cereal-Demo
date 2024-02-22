e42d0635a0dc9b685e0bc25eefbf9e145ce36ae6
stangri2 <stangri2@illinois.edu>
2024-02-22 01:32:39 -0600

    first commit

---
+++ build/CMakeFiles/CEREALDEMO.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CEREALDEMO"
+  "CEREALDEMO.pdb"
+  "CMakeFiles/CEREALDEMO.dir/main.cpp.o"
+  "CMakeFiles/CEREALDEMO.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CEREALDEMO.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
